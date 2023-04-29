472cd1683596362cae40a5b3ded7e00f1f225fc8
Adam Probert <adamprobert@live.co.uk>
2023-04-29 21:50:12 +0100

    Audio added to text

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -7,14 +7,34 @@ public class NPC : MonoBehaviour
     [SerializeField] string dialogueLine;
     [SerializeField] bool debugMode;
 
+    [SerializeField] GameObject dialogueBox;
+    [SerializeField] TypeWriterEffect typeWriterEffect;
+    AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+        dialogueBox.SetActive(false);
+        typeWriterEffect.fullText = dialogueLine;
+        typeWriterEffect.audioSource = audioSource;
+    }
+
     private void OnTriggerEnter(Collider other) 
     {
         if (other.gameObject.tag == "Player")
         {
             if(debugMode){Debug.Log("Player entered NPC trigger.");}
+            dialogueBox.SetActive(true);
+            typeWriterEffect.StartText();
             
-            
-            
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player")
+        {
+            if(debugMode){Debug.Log("Player exited NPC trigger.");}
+            dialogueBox.SetActive(false);
+            typeWriterEffect.Clear();
         }
     }
 }
---
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+    [SerializeField] float delay = 0.1f;
+    public string fullText;
+    TextMeshProUGUI text;
+    public AudioSource audioSource;
+    string currentText = "";
+
+    private void Awake() {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void StartText() {
+        audioSource.Play();
+        StartCoroutine(ShowText());
+    }
+
+    public void Clear() {
+        text.text = "";
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            text.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+        audioSource.Stop();
+    }
+}
