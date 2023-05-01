dd417bc2b34ad8b818f61e34a6b422f79f027e89
Adam Probert <adamprobert@live.co.uk>
2023-05-01 15:28:34 +0100

    Add button audio

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     [SerializeField] GameObject objectToEnable;
+    AudioSource audioSource;
 
+    private void Awake() 
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            audioSource.Play();
             objectToEnable.SendMessage("TurnOn");
         }
     }
